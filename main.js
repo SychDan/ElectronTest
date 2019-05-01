bfe4bce2a2faf46af451726686868c7e59226185
danny <sych.daniil@yandex.ru>
2019-05-01 03:16:35 +0300

    update

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-// Modules to control application life and create native browser window
 const {app, BrowserWindow, Menu, ipcMain, protocol} = require('electron');
 const log = require('electron-log');
 const {autoUpdater} = require('electron-updater');
-// require('update-electron-app')()
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+
 let mainWindow = {};
 autoUpdater.logger = log;
 autoUpdater.requestHeaders = {"PRIVATE_TOKEN": "4c1e59a8bcc4e04b6d30e55720bd82052acbf508"};
 autoUpdater.autoDownload =true;
 autoUpdater.setFeedURL({
   provider: "generic",
-  url: "http://127.0.0.1:9000/"
+  url: "http://<your_host>:9000/"
   // url: "https://gitlab.com/SychDan/electriontest-/jobs/artifacts/master/download/dist?job=build"
 });
 autoUpdater.logger.transports.file.level = "info";
 
 function createWindow () {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -27,18 +23,10 @@ function createWindow () {
   });
 
   mainWindow.webContents.openDevTools();
-  // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html#v${app.getVersion()}`);
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     log.info("Приложение завершено")
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
@@ -48,26 +36,10 @@ function sendStatusToWindow(text) {
   mainWindow.webContents.send('message', text)
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-// app.on('ready', createWindow)
-
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+
   if (process.platform !== 'darwin') app.quit()
 })
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  // if (mainWindow === null) createWindow()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
 autoUpdater.on('checking-for-update', () => {
   log.info('Проверка обновления');
@@ -85,16 +57,15 @@ autoUpdater.on('error', (ev, err) => {
   sendStatusToWindow('Ошибка во время автообновления.');
 })
 autoUpdater.on('download-progress', (ev, progressObj) => {
-  sendStatusToWindow('Download progress...');
+  sendStatusToWindow('Загрузка обновления...');
 })
 autoUpdater.on('update-downloaded', (ev, info) => {
-  sendStatusToWindow('Update downloaded; will install in 5 seconds');
+  sendStatusToWindow('Обновление загружено; оно будет установлено в через 5 секунд');
 });
 
 autoUpdater.on('update-downloaded', (ev, info) => {
-  // Wait 5 seconds, then quit and install
-  // In your application, you don't need to wait 5 seconds.
-  // You could call autoUpdater.quitAndInstall(); immediately
+  // Ожидание 5 секунд прежде чем начать установку
+  // Если не нужно, то можно сразу вызвать autoUpdater.quitAndInstall()
   setTimeout(function() {
     autoUpdater.quitAndInstall();
   }, 5000)

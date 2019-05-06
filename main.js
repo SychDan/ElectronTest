9a661298d7d0b2293648946b7af006d279b4cc21
danny <sych.daniil@yandex.ru>
2019-05-06 21:29:59 +0300

    Update

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ autoUpdater.autoDownload =true;
 autoUpdater.setFeedURL({
   provider: "generic",
   // url: "http://<your_host>:9000/"
-  url: "https://gitlab.com/SychDan/electrontest/-/jobs/artifacts/master/download/dist?job=job_build"
+  url: "https://gitlab.com/SychDan/electrontest/-/jobs/artifacts/master/raw/dist?job=job_build"
 });
 autoUpdater.logger.transports.file.level = "info";
 
@@ -79,82 +79,3 @@ app.on('ready', function() {
   autoUpdater.checkForUpdates()
 });
 
-
-// const {app, BrowserWindow} = require("electron");
-// const updater = require("electron-updater");
-// const autoUpdater = updater.autoUpdater;
-//
-// let mainWindow = {};
-//
-// ///////////////////
-// // Auto upadater //
-// ///////////////////
-// autoUpdater.requestHeaders = { "PRIVATE-TOKEN": "AxshFXC7nExHkC2zNkBw" };
-// autoUpdater.autoDownload = true;
-//
-// autoUpdater.setFeedURL({
-//   provider: "generic",
-//   url: "https://gitlab.com/sychdan/electrontest/-/jobs/artifacts/master/raw/dist?job=build"
-// });
-//
-// autoUpdater.on('checking-for-update', function () {
-//   sendStatusToWindow('Checking for update...');
-// });
-//
-// autoUpdater.on('update-available', function (info) {
-//   sendStatusToWindow('Update available.');
-// });
-//
-// autoUpdater.on('update-not-available', function (info) {
-//   sendStatusToWindow('Update not available.');
-// });
-//
-// autoUpdater.on('error', function (err) {
-//   sendStatusToWindow('Error in auto-updater.');
-// });
-//
-// autoUpdater.on('download-progress', function (progressObj) {
-//   let log_message = "Download speed: " + progressObj.bytesPerSecond;
-//   log_message = log_message + ' - Downloaded ' + parseInt(progressObj.percent) + '%';
-//   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-//   sendStatusToWindow(log_message);
-// });
-//
-// autoUpdater.on('update-downloaded', function (info) {
-//   sendStatusToWindow('Update downloaded; will install in 1 seconds');
-// });
-//
-// autoUpdater.on('update-downloaded', function (info) {
-//   setTimeout(function () {
-//     autoUpdater.quitAndInstall();
-//   }, 1000);
-// });
-//
-// app.on('ready', function() {
-//   createWindow();
-//   autoUpdater.checkForUpdates()
-// });
-//
-// autoUpdater.checkForUpdates();
-//
-// function sendStatusToWindow(message) {
-//   console.log(message);
-//   mainWindow.webContents.send('message', text)
-// }
-//
-// function createWindow () {
-//     mainWindow = new BrowserWindow({
-//         width: 800,
-//         height: 600,
-//         webPreferences: {
-//             nodeIntegration: true
-//         }
-//     });
-//
-//     mainWindow.webContents.openDevTools();
-//   mainWindow.loadURL(`file://${__dirname}/index.html#v${app.getVersion()}`);
-//
-//   mainWindow.on('closed', function () {
-//     mainWindow = null
-//   })
-// }
